50de35d53d4fade77c9ac560973e9092fed2150b
Mx. Valix <65033636+MxValix@users.noreply.github.com>
2020-11-11 09:37:40 +0100

    Update textmining.py

--- textmining.py
+++ textmining.py
@@ -3,11 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-if __name__ == '__main__':
+
+def open_dataset():
     excel_name = 'textmining.xlsx'
     df = pd.read_excel(excel_name)
-    print(df)
+    #print(df)
+    return df
 
+
+def many_trials():
     df
     df.std()
     df.head()
@@ -23,16 +27,31 @@ if __name__ == '__main__':
     df.groupby('Clothing ID')['Rating'].std()
     df['Clothing ID'].unique()
     pd.unique(df['Clothing ID']).tolist()
+    df.groupby('Clothing ID')['Rating'].std()
 
-    plt.figure(figsize=(15, 15))
-    df.groupby(['Department Name', pd.cut(df['Age'], np.arange(0, 100, 10))]) \
-        .size() \
-        .unstack(0) \
-        .plot.bar(stacked=True)
+
+
+def age_plot():
+    x = df['Clothing ID']
+    y = df['Age']
+    plt.plot(x, y, 'bo')
+    plt.title('Età per prodotto')
+    plt.xlabel('IdProdotto')
+    plt.ylabel('Età')
+    plt.legend(['Age'])
     plt.show()
 
-    z = df.groupby(by=['Department Name'], as_index=False).count().sort_values(by='Class Name', ascending=False)
 
+def department_age_plot():
+    plt.figure(figsize=(15, 15))
+    a = df.groupby(['Department Name', pd.cut(df['Age'], np.arange(0, 100, 10))])
+    a.size().unstack(0).plot.bar(stacked=True)
+    plt.show()
+
+
+def counts_department_plot():
+    z = df.groupby(by=['Department Name'], as_index=False)
+    z = z.count().sort_values(by='Class Name', ascending=False)
     plt.figure(figsize=(10, 10))
     sns.set_style("whitegrid")
     ax = sns.barplot(x=z['Department Name'], y=z['Class Name'], data=z, palette='plasma')
@@ -42,18 +61,13 @@ if __name__ == '__main__':
     plt.show()
 
 
-    x = df['Clothing ID']
-    y = df['Age']
-
-    plt.plot(x, y, 'bo')
-    plt.title('Età per prodotto')
-    plt.xlabel('IdProdotto')
-    plt.ylabel('Età')
-
+def rating_age_plot():
+    b = df.groupby(['Rating', pd.cut(df['Age'], np.arange(0, 100, 10))])
+    b.size().unstack(0).plot.bar(stacked=True)
     plt.show()
 
-    df.groupby('Clothing ID')['Rating'].std()
 
+def department_productid():
     df['Age'].plot(kind='hist', color='teal')
     department_name = df['Department Name']
 
@@ -72,13 +86,19 @@ if __name__ == '__main__':
     plt.title('Dipartimento ')
     plt.xlabel('IdProdotto')
     plt.ylabel('Dipartimento')
-    plt.xlim([15,90])
+    plt.xlim([15, 90])
     plt.show()
 
-    df.groupby(['Rating', pd.cut(df['Age'], np.arange(0, 100, 10))]) \
-        .size() \
-        .unstack(0) \
-        .plot.bar(stacked=True)
-    plt.show()
+
+if __name__ == '__main__':
+    df = open_dataset()
+    many_trials()
+    age_plot()
+    department_age_plot()
+    counts_department_plot()
+    rating_age_plot()
+    department_productid()
+
+
 
 
